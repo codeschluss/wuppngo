0f4f5ff9edfeee372f99d7ef8133020184cc5bcf
Pseipel <Pseipel@web.de>
2018-11-28 15:32:05 +0100

    init blog and search components

---
+++ client/src/views/public/search/searchresult..list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'search-list-component',
+    styleUrls: [],
+    templateUrl: 'searchresult.list.component.html'
+})
+
+export class SearchResultListComponent {
+
+  public static readonly imports = [];
+
+  constructor(
+  ) {
+  }
+
+}
