25050dad741e99725718008e73618910a5477095
Pseipel <Pseipel@web.de>
2018-12-15 16:58:44 +0100

    fixed merge conflicts

--- client/src/views/public/organisation/organisation.view.component.ts
+++ client/src/views/public/organisation/organisation.view.component.ts
@@ -1,12 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { OrganisationModel } from 'src/realm/organisation/organisation.model';
 import { ActivityModel } from 'src/realm/activity/activity.model';
-import { AddressModel } from 'src/realm/address/address.model';
-import { SuburbModel } from 'src/realm/suburb/suburb.model';
-import { CategoryModel } from 'src/realm/category/category.model';
-import { TargetGroupModel } from 'src/realm/target-group/target-group.model';
-import { ScheduleModel } from 'src/realm/schedule/schedule.model';
 import { OrganisationImageModel } from 'src/realm/image/organisation-image.model';
 import { MatDialog } from '@angular/material';
 import { OrgaMediaDialogComponent } from './organisation.mediacontent.dialog.component';
@@ -83,111 +77,5 @@ export class OrganisationViewComponent {
         'https://www.youtube.com/embed/'));
     }
   }
-
-
-  buildTestData(): OrganisationModel {
-    const organisation = new OrganisationModel();
-
-    // organisation.id = 'testActivity';
-    organisation.name = 'FakeActivity';
-    organisation.mail = 'FakeActivity@internet.de';
-
-    organisation.description = 'This is just a FakeActivity to show'
-      + 'how this could look like.';
-    const testAddress = new AddressModel();
-    testAddress.street = 'samplestreet';
-    testAddress.houseNumber = '42a';
-    testAddress.latitude = 51.00;
-    testAddress.longitude = 7.00;
-    testAddress.postalCode = '63628';
-
-    const testSubUrb = new SuburbModel();
-    testSubUrb.name = 'Elberfeld';
-    // testSubUrb.id = '1';
-
-    testAddress.suburb = new Promise<SuburbModel>((resolve, reject) => {
-      resolve(testSubUrb);
-    });
-
-    testAddress.place = 'SampleCity';
-    organisation.address = new Promise<AddressModel>((resolve, reject) => {
-      resolve(testAddress);
-    });
-
-    return organisation;
-  }
-
-  buildTestActivity(): ActivityModel {
-    const actOne = new ActivityModel;
-    // actOne.id = 'testActivity';
-    actOne.name = 'FakeActivity';
-    actOne.description = 'This is just a FakeActivity to show'
-      + 'how this could look like.';
-    const testAddress = new AddressModel();
-    testAddress.street = 'samplestreet';
-    testAddress.houseNumber = '42a';
-    testAddress.latitude = 51.00;
-    testAddress.longitude = 7.00;
-    testAddress.postalCode = '63628';
-
-    const testSubUrb = new SuburbModel();
-    testSubUrb.name = 'Elberfeld';
-    // testSubUrb.id = '1';
-
-    testAddress.suburb = new Promise<SuburbModel>((resolve, reject) => {
-      resolve(testSubUrb);
-    });
-
-    testAddress.place = 'SampleCity';
-    actOne.address = new Promise<AddressModel>((resolve, reject) => {
-      resolve(testAddress);
-    });
-
-    const category = new CategoryModel;
-    category.name = 'party';
-    category.color = 'blue';
-
-    actOne.category = new Promise<CategoryModel>((resolve, reject) => {
-      resolve(category);
-    });
-
-    const target_group = new TargetGroupModel;
-    target_group.name = 'youth';
-    const targetGroups = [target_group];
-
-    actOne.targetGroups = new Promise<TargetGroupModel[]>((resolve, reject) => {
-      resolve(targetGroups);
-    });
-
-    const schedule = new ScheduleModel;
-    schedule.startDate = new Date().toUTCString();
-    schedule.endDate = new Date().toUTCString();
-    const schedules = [schedule];
-
-    const firstDate = new ScheduleModel;
-    firstDate.startDate = new Date().toISOString();
-    firstDate.endDate =
-      new Date(new Date(firstDate.startDate).getDate() + 1).toISOString();
-
-      const secondDate = new ScheduleModel;
-    secondDate.startDate = new Date(new Date().getDate() + 7).toISOString();
-    secondDate.endDate =
-      new Date(new Date(secondDate.startDate).getDate() + 1).toISOString();
-
-      schedules.push(firstDate);
-    schedules.push(secondDate);
-
-    actOne.schedules = new Promise<ScheduleModel[]>((resolve, reject) => {
-      resolve(schedules);
-    });
-
-    const organisation = new OrganisationModel;
-    organisation.name = 'testOrganisation';
-    actOne.organisation = new Promise<OrganisationModel>((resolve, reject) => {
-      resolve(organisation);
-    });
-
-    return actOne;
-}
 
 }
--- client/src/views/public/search/searchresult.list.component.ts
+++ client/src/views/public/search/searchresult.list.component.ts
@@ -37,147 +37,147 @@ export class SearchResultListComponent {
     // organisations and blogs in 3 seperated API calls
 
     // Just for testing
-    for (let i = 0; i < 20; i++) {
-      this.activities.push(this.buildTestActivity());
-    }
+    // for (let i = 0; i < 20; i++) {
+    //   this.activities.push(this.buildTestActivity());
+    // }
 
-    for (let i = 0; i < 20; i++) {
-      this.organisations.push(this.buildTestOrgaData());
-    }
+    // for (let i = 0; i < 20; i++) {
+    //   this.organisations.push(this.buildTestOrgaData());
+    // }
 
-    for (let i = 0; i < 20; i++) {
-      this.blogs.push(this.buildTestBlog());
-    }
+    // for (let i = 0; i < 20; i++) {
+    //   this.blogs.push(this.buildTestBlog());
+    // }
   }
 
-  buildTestActivity(): ActivityModel {
-    const actOne = new ActivityModel;
-    actOne.id = 'testActivity';
-    actOne.name = 'FakeActivity';
-    actOne.description = 'This is just a FakeActivity to show'
-      + 'how this could look like.';
-    const testAddress = new AddressModel();
-    testAddress.street = 'samplestreet';
-    testAddress.houseNumber = '42a';
-    testAddress.latitude = 51.00;
-    testAddress.longitude = 7.00;
-    testAddress.postalCode = '63628';
+  // buildTestActivity(): ActivityModel {
+  //   const actOne = new ActivityModel;
+  //   actOne.id = 'testActivity';
+  //   actOne.name = 'FakeActivity';
+  //   actOne.description = 'This is just a FakeActivity to show'
+  //     + 'how this could look like.';
+  //   const testAddress = new AddressModel();
+  //   testAddress.street = 'samplestreet';
+  //   testAddress.houseNumber = '42a';
+  //   testAddress.latitude = 51.00;
+  //   testAddress.longitude = 7.00;
+  //   testAddress.postalCode = '63628';
 
-    const testSubUrb = new SuburbModel();
-    testSubUrb.name = 'Elberfeld';
-    testSubUrb.id = '1';
+  //   const testSubUrb = new SuburbModel();
+  //   testSubUrb.name = 'Elberfeld';
+  //   testSubUrb.id = '1';
 
-    testAddress.suburb = new Promise<SuburbModel>((resolve, reject) => {
-      resolve(testSubUrb);
-    });
+  //   testAddress.suburb = new Promise<SuburbModel>((resolve, reject) => {
+  //     resolve(testSubUrb);
+  //   });
 
-    testAddress.place = 'SampleCity';
-    actOne.address = new Promise<AddressModel>((resolve, reject) => {
-      resolve(testAddress);
-    });
+  //   testAddress.place = 'SampleCity';
+  //   actOne.address = new Promise<AddressModel>((resolve, reject) => {
+  //     resolve(testAddress);
+  //   });
 
-    const category = new CategoryModel;
-    category.name = 'party';
-    category.color = 'blue';
+  //   const category = new CategoryModel;
+  //   category.name = 'party';
+  //   category.color = 'blue';
 
-    actOne.category = new Promise<CategoryModel>((resolve, reject) => {
-      resolve(category);
-    });
+  //   actOne.category = new Promise<CategoryModel>((resolve, reject) => {
+  //     resolve(category);
+  //   });
 
-    const target_group = new TargetGroupModel;
-    target_group.name = 'youth';
-    const targetGroups = [target_group];
+  //   const target_group = new TargetGroupModel;
+  //   target_group.name = 'youth';
+  //   const targetGroups = [target_group];
 
-    actOne.targetGroups =
-      new Promise<TargetGroupModel[]>((resolve, reject) => {
-      resolve(targetGroups);
-    });
+  //   actOne.targetGroups =
+  //     new Promise<TargetGroupModel[]>((resolve, reject) => {
+  //     resolve(targetGroups);
+  //   });
 
-    const schedule = new ScheduleModel;
-    schedule.startDate = new Date().toUTCString();
-    schedule.endDate = new Date().toUTCString();
-    const schedules = [schedule];
+  //   const schedule = new ScheduleModel;
+  //   schedule.startDate = new Date().toUTCString();
+  //   schedule.endDate = new Date().toUTCString();
+  //   const schedules = [schedule];
 
-    const firstDate = new ScheduleModel;
-    firstDate.startDate = new Date().toISOString();
-    firstDate.endDate =
-      new Date(new Date(firstDate.startDate).getDate() + 1).toISOString();
+  //   const firstDate = new ScheduleModel;
+  //   firstDate.startDate = new Date().toISOString();
+  //   firstDate.endDate =
+  //     new Date(new Date(firstDate.startDate).getDate() + 1).toISOString();
 
-      const secondDate = new ScheduleModel;
-    secondDate.startDate = new Date(new Date().getDate() + 7).toISOString();
-    secondDate.endDate =
-      new Date(new Date(secondDate.startDate).getDate() + 1).toISOString();
+  //     const secondDate = new ScheduleModel;
+  //   secondDate.startDate = new Date(new Date().getDate() + 7).toISOString();
+  //   secondDate.endDate =
+  //     new Date(new Date(secondDate.startDate).getDate() + 1).toISOString();
 
-      schedules.push(firstDate);
-    schedules.push(secondDate);
+  //     schedules.push(firstDate);
+  //   schedules.push(secondDate);
 
-    actOne.schedules = new Promise<ScheduleModel[]>((resolve, reject) => {
-      resolve(schedules);
-    });
+  //   actOne.schedules = new Promise<ScheduleModel[]>((resolve, reject) => {
+  //     resolve(schedules);
+  //   });
 
-    const organisation = new OrganisationModel;
-    organisation.name = 'testOrganisation';
-    actOne.organisation =
-      new Promise<OrganisationModel>((resolve, reject) => {
-      resolve(organisation);
-    });
+  //   const organisation = new OrganisationModel;
+  //   organisation.name = 'testOrganisation';
+  //   actOne.organisation =
+  //     new Promise<OrganisationModel>((resolve, reject) => {
+  //     resolve(organisation);
+  //   });
 
-    return actOne;
-  }
+  //   return actOne;
+  // }
 
-  buildTestOrgaData(): OrganisationModel {
-    const organisation = new OrganisationModel();
+  // buildTestOrgaData(): OrganisationModel {
+  //   const organisation = new OrganisationModel();
 
-    organisation.id = 'testActivity';
-    organisation.name = 'FakeActivity';
-    organisation.mail = 'FakeActivity@internet.de';
+  //   organisation.id = 'testActivity';
+  //   organisation.name = 'FakeActivity';
+  //   organisation.mail = 'FakeActivity@internet.de';
 
-    organisation.description = 'This is just a FakeActivity to show'
-      + 'how this could look like.';
-    const testAddress = new AddressModel();
-    testAddress.street = 'samplestreet';
-    testAddress.houseNumber = '42a';
-    testAddress.latitude = 51.00;
-    testAddress.longitude = 7.00;
-    testAddress.postalCode = '63628';
+  //   organisation.description = 'This is just a FakeActivity to show'
+  //     + 'how this could look like.';
+  //   const testAddress = new AddressModel();
+  //   testAddress.street = 'samplestreet';
+  //   testAddress.houseNumber = '42a';
+  //   testAddress.latitude = 51.00;
+  //   testAddress.longitude = 7.00;
+  //   testAddress.postalCode = '63628';
 
-    const testSubUrb = new SuburbModel();
-    testSubUrb.name = 'Elberfeld';
-    testSubUrb.id = '1';
+  //   const testSubUrb = new SuburbModel();
+  //   testSubUrb.name = 'Elberfeld';
+  //   testSubUrb.id = '1';
 
-    testAddress.suburb = new Promise<SuburbModel>((resolve, reject) => {
-      resolve(testSubUrb);
-    });
+  //   testAddress.suburb = new Promise<SuburbModel>((resolve, reject) => {
+  //     resolve(testSubUrb);
+  //   });
 
-    testAddress.place = 'SampleCity';
-    organisation.address = new Promise<AddressModel>((resolve, reject) => {
-      resolve(testAddress);
-    });
+  //   testAddress.place = 'SampleCity';
+  //   organisation.address = new Promise<AddressModel>((resolve, reject) => {
+  //     resolve(testAddress);
+  //   });
 
-    return organisation;
-  }
+  //   return organisation;
+  // }
 
-  buildTestBlog(): BlogModel {
-    const blog = new BlogModel;
-    blog.author = 'Franz test';
-    blog.creationDate = new Date().toDateString();
-    blog.postText = 'Lorem ipsum ' +
-    'dolor sit amet, consetetur sadipscing elitr, sed ' +
-    'diam nonumy eirmod tempor invidunt ut labore et ' +
-    'dolore magna aliquyam erat, sed diam voluptua. At ' +
-    'vero eos et accusam et justo duo dolores et ea rebum. ' +
-    'Stet clita kasd gubergren, no sea takimata sanctus est ' +
-    'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, ' +
-    'consetetur sadipscing elitr, sed diam nonumy eirmod tempor ' +
-    'invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' +
-    'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita ' +
-    'kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
-    blog.title = 'Great Test Blogpost';
-    blog.activity =  new Promise<ActivityModel>((resolve, reject) => {
-      resolve(this.buildTestActivity());
-    });
-    return blog;
-  }
+  // buildTestBlog(): BlogModel {
+  //   const blog = new BlogModel;
+  //   blog.author = 'Franz test';
+  //   blog.creationDate = new Date().toDateString();
+  //   blog.postText = 'Lorem ipsum ' +
+  //   'dolor sit amet, consetetur sadipscing elitr, sed ' +
+  //   'diam nonumy eirmod tempor invidunt ut labore et ' +
+  //   'dolore magna aliquyam erat, sed diam voluptua. At ' +
+  //   'vero eos et accusam et justo duo dolores et ea rebum. ' +
+  //   'Stet clita kasd gubergren, no sea takimata sanctus est ' +
+  //   'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, ' +
+  //   'consetetur sadipscing elitr, sed diam nonumy eirmod tempor ' +
+  //   'invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' +
+  //   'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita ' +
+  //   'kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+  //   blog.title = 'Great Test Blogpost';
+  //   blog.activity =  new Promise<ActivityModel>((resolve, reject) => {
+  //     resolve(this.buildTestActivity());
+  //   });
+  //   return blog;
+  // }
 
 
 
