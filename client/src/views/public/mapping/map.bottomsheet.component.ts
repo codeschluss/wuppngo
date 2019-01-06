b76d728c9888446a231f1e4f72cc46fd5510dabe
Pseipel <Pseipel@web.de>
2019-01-06 23:16:03 +0100

    styling cookies message

--- client/src/views/public/mapping/map.bottomsheet.component.ts
+++ client/src/views/public/mapping/map.bottomsheet.component.ts
@@ -1,18 +1,25 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA } from '@angular/material';
-import { ActivityModel } from '../../../realm/activity/activity.model';
-import { ConfigurationProvider } from 'src/realm/configuration/configuration.provider';
 import { ConfigurationModel } from 'src/realm/configuration/configuration.model';
+import { ConfigurationProvider } from 'src/realm/configuration/configuration.provider';
+import { ActivityModel } from '../../../realm/activity/activity.model';
 
 @Component({
   selector: 'bottom-sheet',
-  template:
-    '<div id="largeMap" *ngIf="configurations; else loading">'
-    + '<mapping-component [activities] = activities '
-    + '[configurations] = configurations '
-    + '[disableCarousel] = "true">'
-    + '</mapping-component></div>'
-    + '<ng-template #loading><i18n>waitingForMap</i18n></ng-template>',
+  template: `
+  <div id="largeMap" *ngIf="configurations; else loading">
+      <mapping-component
+        [activities] = activities
+        [configurations] = configurations
+        [disableCarousel] = "true">
+      </mapping-component>
+    </div>
+    <ng-template #loading>
+      <i18n [style.width]="'100%'"
+      [style.text-align]="'center'">
+        waitingForMap
+      </i18n>
+  </ng-template>`,
   styleUrls: ['map.bottomsheet.component.css']
 })
 
